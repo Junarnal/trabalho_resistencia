2f810284c1c0bbc3eee535c869490a430cfd6d16
milton <milton.aptab@gmail.com>
2025-05-06 09:18:22 -0400

    calc

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+
 
 class Retangulo:
     def __init__(self, base, altura, cx, cy, subtrair=False):
@@ -34,50 +36,50 @@ class Retangulo:
     def momento_polar_origem(self):
        return (momento_eixo_x + momento_eixo_y)
 
-class Plano_Cartesiano:
-    def __init__(self):
-        self.fig, self.ax = plt.subplots(figsize=(10, 10))
-        #fig.patch.set_facecolor('#ffffff')      
-        self.ax.set_xlim(-10, 10)
-        self.ax.set_ylim(-10, 10)
-        self.ax.spines['bottom'].set_position('zero')
-        self.ax.spines['left'].set_position('zero')
-        self.ax.spines['right'].set_color('none')
-        self.ax.spines['top'].set_color('none')
-        self.ax.set_aspect('equal')
-        self.ax.grid(True)
-        self.fig.canvas.mpl_connect('button_press_event', self.on_press)
-        self.fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
-        self.fig.canvas.mpl_connect('button_release_event', self.on_release)
-        self.press = False
-        self.mouse_press = None
-        self.xlim_press = None
-        self.ylim_press = None
 
-    def on_press(self, event):
-        self.mouse_press = event
-        self.xlim_press = self.ax.get_xlim()
-        self.ylim_press = self.ax.get_ylim()
-        self.press = True
+def make_error_boxes(ax, x, y, base, altura, facecolor='g',
+                     edgecolor='none', alpha=0.75):
 
-    def on_motion(self, event):
-        if(self.press):
-            delta_x = event.xdata - self.mouse_press.xdata
-            delta_y = event.ydata - self.mouse_press.ydata
-            new_xlim = (self.xlim_press[0] - delta_x, self.xlim_press[1] - delta_x)
-            new_ylim = (self.ylim_press[0] - delta_x, self.ylim_press[1] - delta_y)
+    # Loop over data points; create box from errors at each point
+    errorboxes = [Rectangle((x-base/2, y-altura/2), base, altura)]
 
-            self.ax.set_xlim(new_xlim)
-            self.ax.set_ylim(new_ylim)
-            print(f'new_xlim: {new_xlim}, new_ylim: {new_ylim}')
-            self.fig.canvas.draw()
-            #print(delta_x, delta_y)
+    # Create patch collection with specified colour/alpha
+    pc = PatchCollection(errorboxes, facecolor=facecolor, alpha=alpha,
+                         edgecolor=edgecolor)
 
-    def on_release(self, event):
-        self.press = False
+    # Add collection to Axes
+    ax.add_collection(pc)
 
-    def run(self):
-        plt.show()
+    # Plot errorbars
+    artists = errorboxes
 
-plano = Plano_Cartesiano()
-plano.run()
+    return artists
+
+
+xmin = -5
+xmax = 5
+ymin = -5
+ymax = 5
+plt.rcParams['toolbar'] = 'none'
+
+fig, ax = plt.subplots(figsize=(10, 10))
+#fig.patch.set_facecolor('#ffffff')
+
+
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.spines['bottom'].set_position('zero')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.set_aspect('equal')
+ax.grid(True)
+base=2
+altura=3
+x=2
+y=2
+_ = make_error_boxes(ax, x, y, base, altura)
+
+
+plt.show()
+
